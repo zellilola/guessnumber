88f5635721d74b8f5c2eff096ce892f2c46883c6
zellilola <zellilola@gmail.com>
2021-03-07 17:36:57 +0300

    program basic logic

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
 
 guessed_number = random.randint(0, 100)
-print(guessed_number)
+user_number = int(input('Введите ваше число: '))
+if user_number < guessed_number:
+    print('Ваше число меньше загаданного')
+elif user_number > guessed_number:
+    print('Ваше число больше загаданного')
+else:
+    print('Вы угадали число')
