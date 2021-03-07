1e8587ab0739d786c7af3520ee3815fd01755178
zellilola <zellilola@gmail.com>
2021-03-07 17:57:14 +0300

    refactoring

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import random
 
 guessed_number = random.randint(0, 100)
-user_number = int(input('Введите ваше число: '))
-if user_number < guessed_number:
-    print('Ваше число меньше загаданного')
-elif user_number > guessed_number:
-    print('Ваше число больше загаданного')
+
+attempts = 0
+
+max_attempts = 10
+
+while attempts < max_attempts:
+    user_number = int(input('Введите ваше число: '))
+    if guessed_number == user_number:
+        print('Вы угадали число')
+        break
+    attempts += 1
+    if user_number < guessed_number:
+        print('Ваше число меньше загаданного')
+    else:
+        print('Ваше число больше загаданного')
+
 else:
-    print('Вы угадали число')
+    print('Вы не угадали')
+
+
+
